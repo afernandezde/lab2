ad62f33e31fc9f4bfea58a5bdbadb1ecf412e484
Alex Fernandez <afernandez@example.com>
2025-12-04 22:40:39 +0100

    correcciones

--- backend/src/test/java/com/tecnocampus/LS2/protube_back/controller/PlaylistControllerTest.java
+++ backend/src/test/java/com/tecnocampus/LS2/protube_back/controller/PlaylistControllerTest.java
@@ -5,8 +5,7 @@ import com.tecnocampus.LS2.protube_back.domain.Playlist;
 import com.tecnocampus.LS2.protube_back.services.PlaylistService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,8 +18,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(PlaylistController.class)
 class PlaylistControllerTest {
 
     @Autowired
